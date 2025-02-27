abe7bd1dd0d072902687745a9fdd48dc30e04ee8
Flavi Bumba <30435407+YekolaAcademy@users.noreply.github.com>
2025-02-27 23:06:00 +0000

    new changes

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer (){
+    return <div style={{backgroundColor: 'brown', marginTop: '20px', padding: '30px', textAlign: 'center'}}>
+       
+        <p> Copyright &copy; All Rights Reserved
+        <script>document.write( new Date().getFullYear() ); </script>
+        | Designed By Flavi Bumba  </p>
+        
+    </div>
+}
+export default Footer
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+function Navbar() {
+
+    return <div style={{backgroundColor: 'brown', margin: '20px', padding: '20px', textAlign: 'center'}}> 
+        <h1>Seafood App</h1> 
+        <a style={{margin:'10px', color:'white'}}>Home</a>
+        <a style={{margin:'10px', color:'white'}}>Order</a>
+        <a style={{margin:'10px', color:'white'}}>Contact Us</a>
+    
+    
+    </div>
+     
+}
+export default Navbar
